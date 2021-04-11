b5d8db188f805ffc813749baa4baee3185b68427
Staale Dahl <stalehd@gmail.com>
2021-04-12 01:42:44 +0200

    Update profile example

--- examples/README.md
+++ examples/README.md
@@ -2,3 +2,5 @@
 
 This directory contains examples for the Rust client library.
 
+Note that the generated code (currently) has no support for TLS so you have to
+include some additional dependencies in your `Cargo.toml` file.
--- examples/profile/src/main.rs
+++ examples/profile/src/main.rs
@@ -1,29 +1,39 @@
-use userapi::apis::configuration;
-use userapi::apis::profile_api::{user_get_user_profile};
-use futures::executor::block_on;
-use userapi::apis::configuration::ApiKey;
 use std::env;
+use futures::executor::block_on;
+use userapi::apis::configuration;
+use userapi::apis::configuration::ApiKey;
+use userapi::apis::profile_api::{user_get_user_profile};
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    // Get the token from the command line.
     let args: Vec<String> = env::args().collect();
-
     if args.len() < 2 {
         panic!("Needs a token")
     }
 
+    // Create the api key struct for the token
     let api_key = ApiKey{
         prefix: None,
         key: String::from(&args[1]),
     };
 
+    // Create configuration and add the API key struct to it.
     let mut config = configuration::Configuration::new();
     config.api_key = Some(api_key);
 
+    // Finally -- get the profile
     let profile_result = user_get_user_profile(&config);
 
+    // ..and print the result
     match block_on(profile_result) {
         Ok(resp) => {
-            println!("Success! Profile = {:?}", resp);
+            println!("Profile");
+            println!("=======");
+            println!("User ID: {}", resp.user_id.unwrap_or_default());
+            println!("Name:    {}", resp.name.unwrap_or_default());
+            println!("Email:   {}", resp.email.unwrap_or_default());
+            println!("Avatar:  {}", resp.avatar_url.unwrap_or_default());
         },
         Err(error) => {
             println!("error: {:?}", error);
